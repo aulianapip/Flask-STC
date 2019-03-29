74da62fe1cf15267189df0b9e1232ede7b023b42
novanggrini <novaanggraini95@gmail.com>
2019-03-29 20:16:10 +0700

    tanbah beberapa persiapan function

--- app/module/controller.py
+++ app/module/controller.py
@@ -23,9 +23,28 @@ def notfound(error):
 def upload():
     return render_template("upload.html")
 
+@app.route('/pilih_data', methods= ['GET','POST'])
+def pilih_data():
+    return render_template("pilih_data.html")
 
+@app.route('/tampil_data', methods= ['GET','POST'])
+def tampil_data():
+    return render_template("tampil_data.html")
 
+@app.route('/stopword', methods= ['GET','POST'])
+def stopword():
+    return render_template("stopword.html")
 
+@app.route('/stemming', methods= ['GET','POST'])
+def stemming():
+    return render_template("stemming.html")
 
+@app.route('/stc', methods= ['GET','POST'])
+def stc():
+    return render_template("stc.html")
+
+@app.route('/hasil_cluster', methods= ['GET','POST'])
+def hasil_cluster():
+    return render_template("hasil_cluster.html")
 
 
